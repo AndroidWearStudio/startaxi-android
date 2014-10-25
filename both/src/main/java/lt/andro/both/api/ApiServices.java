59f31661359f4d40b702fa24576b149059753107
Vilius Kraujutis <viliusk@gmail.com>
2014-10-25 13:24:03 +0300

    Add RxJava dependency

--- both/src/main/java/lt/andro/both/api/ApiServices.java
+++ both/src/main/java/lt/andro/both/api/ApiServices.java
@@ -1,13 +1,13 @@
 package lt.andro.both.api;
 
 import lt.andro.both.api.entity.TaxiProviderResponse;
-import retrofit.Callback;
 import retrofit.http.GET;
+import rx.Observable;
 
 /**
  * @author Vilius Kraujutis
  * @since 2014-10-25 12:58
  */
 public interface ApiServices {
-    @GET("/taxi/provider") public void getTaxiProvider(Callback<TaxiProviderResponse> callback);
+    @GET("/taxi/provider") public Observable<TaxiProviderResponse> getTaxiProvider();
 }
