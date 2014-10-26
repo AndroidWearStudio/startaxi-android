f7917d0249bee73577a924e1073afe33e4afef02
Andrius Semionovas <aneworld@gmail.com>
2014-10-26 15:16:04 +0200

    Stubbed providers

--- wear/src/main/java/lt/andro/startaxi/activities/ProvidersActivity.java
+++ wear/src/main/java/lt/andro/startaxi/activities/ProvidersActivity.java
@@ -1,26 +1,48 @@
 package lt.andro.startaxi.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import java.util.List;
-
+import android.support.wearable.view.GridViewPager;
+import android.view.View;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import lt.andro.both.api.entity.Estimate;
 import lt.andro.both.api.entity.PossibleJourneys;
+import lt.andro.startaxi.R;
+import lt.andro.startaxi.views.CardAdapter;
+import lt.andro.startaxi.views.CardItemView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Andrius Semionovas
  * @since 2014-10-26
  */
 public class ProvidersActivity extends BaseActivity {
+    @InjectView(R.id.activity_destination_chooser_pager)
+    GridViewPager pager;
+
     public final static String EXTRA_DESTINATION = "destination";
     private DataCommunicator dataCommunicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination_chooser);
+        ButterKnife.inject(this);
 
-        loadProviders("Home");
-        dataCommunicator = new DataCommunicator(this);
+        List<View> cards = new ArrayList<View>(2);
+        cards.add(CardItemView.createCard(this, "Blue taxi", "Arrives in 15 min", R.drawable.ic_launcher));
+        cards.add(CardItemView.createCard(this, "Red taxi", "Arrives in 10 min", R.drawable.ic_launcher));
+
+        pager.setAdapter(new CardAdapter(cards));
+        cards.get(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProvidersActivity.this, ProvidersActivity.class));
+            }
+        });
     }
 
     private void loadProviders(String address) {
@@ -34,7 +56,5 @@ public class ProvidersActivity extends BaseActivity {
     }
 
     private void loadedEstimates(List<Estimate> estimates) {
-        // will turn off indeterminate loader and display data
-        // Andrius
     }
 }
