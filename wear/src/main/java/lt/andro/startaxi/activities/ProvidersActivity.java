2934b5733c2e043c4531db78bf8e2f750c3d9e1a
Andrius Semionovas <aneworld@gmail.com>
2014-10-26 13:21:07 +0200

    Added stub ProvidersActivity

---
+++ wear/src/main/java/lt/andro/startaxi/activities/ProvidersActivity.java
@@ -0,0 +1,36 @@
+package lt.andro.startaxi.activities;
+
+import android.os.Bundle;
+import lt.andro.both.api.entity.Estimate;
+import lt.andro.both.api.entity.PossibleJourneys;
+
+import java.util.List;
+
+/**
+ * @author Andrius Semionovas
+ * @since 2014-10-26
+ */
+public class ProvidersActivity extends BaseActivity {
+    public final static String EXTRA_DESTINATION = "destination";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loadProviders("Home");
+    }
+
+    private void loadProviders(String address) {
+        //here need to convert address to possible journeys
+
+        loadProviders(new PossibleJourneys());
+    }
+
+    private void loadProviders(PossibleJourneys possibleJourneys) {
+        //load estimations and pass to next method
+    }
+
+    private void loadedEstimates(List<Estimate> estimates) {
+        
+    }
+}
